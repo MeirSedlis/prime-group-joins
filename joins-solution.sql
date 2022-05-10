a2d85f3e79dafa5c1b8f16b500042e5b86123bbe
Meir Sedlis <meirsedlis@gmail.com>
2022-05-10 13:33:01 -0500

    basemode smashed

--- joins-solution.sql
+++ joins-solution.sql
@@ -24,9 +24,30 @@ JOIN "products" ON products.id = warehouse_product.product_id
 WHERE products.description = 'diet pepsi';
 
 -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT customers.first_name, count("orders")
+FROM "orders"
+JOIN "addresses" ON orders.address_id = addresses.id
+JOIN "customers" ON customers.id = addresses.customer_id
+GROUP BY customers.first_name;
+
+-- WHY IS THIS THROWING AN ERROR WHEN I TRY TO INCLUDE THE CUSTOMER'S LAST NAME??
+    -- SELECT customers.first_name, customers.last_name, count("orders")
+    -- FROM "orders"
+    -- JOIN "addresses" ON orders.address_id = addresses.id
+    -- JOIN "customers" ON customers.id = addresses.customer_id
+    -- GROUP BY customers.first_name;
 
 -- 6. How many customers do we have?
+SELECT count(*) 
+FROM customers;
 
 -- 7. How many products do we carry?
+SELECT count(*) 
+FROM products;
 
 -- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT products.description, sum(warehouse_product.on_hand)
+FROM "products"
+JOIN "warehouse_product" on products.id = warehouse_product.product_id
+WHERE products.description = 'diet pepsi'
+GROUP BY products.description;
